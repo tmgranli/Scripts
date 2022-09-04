5c4b0b4e38a2aa1a98c8af13c75f4c3e0f80ba74
tmgranli <tm.granli@gmail.com>
2022-09-04 22:32:18 +0200

    Update

--- Proactive remediations/Invoke-Remediation-kiosk.ps1
+++ Proactive remediations/Invoke-Remediation-kiosk.ps1
@@ -15,7 +15,8 @@
     Updated:     2022-04-09
     
     Version history:
-    1.0.0 - (2022-04-09) Script created
+    1.0.1 - (2022-04-09) Script created
+    1.0.2 - (2022-04-09) Added logic to detec if user exist or needs to be created.
 #>    
 
 # Random Password Generator
@@ -45,34 +46,60 @@ function Get-TimeStamp {
     
 $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 $DefaultUsername = "KIOSK"
-$ConfigLog = 'C:\Sarpsborg kommune\Logs\UserConfigLog.txt'
+$ConfigLog = 'C:\Sarpsborg kommune\Logs\Log-KioskUserConfig.txt'
 $DetectionMethod = 'C:\Sarpsborg kommune\DetectionMethod\KioskUserCreated_v1.txt'
 
 Try {
 
-    #Create local user for KIOSK
-    New-LocalUser -Name $DefaultUsername -AccountNeverExpires:$true -UserMayNotChangePassword:$true -PasswordNeverExpires:$true -Password ( ConvertTo-SecureString -AsPlainText -Force $scrambledpassword ) -FullName $DefaultUsername -Description "Local KIOSK User" -ErrorAction Stop -Verbose
-    Add-Content $ConfigLog "$(Get-TimeStamp) - Local 'KIOSK' user have been created." -Verbose
-    Write-Verbose "User: $DefaultUsername was created" -Verbose
+
+    if (Get-LocalUser -Name 'KIOSK') {
+        Write-Host 'User Found, creating new password'
+        Add-Content $ConfigLog "$(Get-TimeStamp) - Local 'KIOSK' user already exist - Creating new password."
+
+        Set-LocalUser -Name KIOSK -Password ( ConvertTo-SecureString -AsPlainText -Force $scrambledpassword ) -Verbose -ErrorAction Stop
+        Add-Content $ConfigLog "$(Get-TimeStamp) - User: 'KIOSK' is happy with it's new passw0rd."
+        Write-Verbose "User: 'KIOSK' is happy with it's new passw0rd. " -Verbose
+
+        Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String -ErrorAction Stop -Verbose
+        Add-Content $ConfigLog "$(Get-TimeStamp) - Making sure AutoAdminLogon is set to '1'."
+        Write-Verbose "Making sure AutoAdminLogon is set to '1' " -Verbose
+
+        Set-ItemProperty $RegPath "DefaultUsername" -Value ".\$DefaultUsername" -type String  -ErrorAction Stop -Verbose
+        Add-Content $ConfigLog "$(Get-TimeStamp) - Making sure $DefaultUsername is configured in registry."
+        Write-Verbose "Making sure $DefaultUsername is configured in registry" -Verbose
+
+        Set-ItemProperty $RegPath "DefaultPassword" -Value "$scrambledpassword" -type String -ErrorAction Stop -Verbose
+        Add-Content $ConfigLog "$(Get-TimeStamp) - Making sure DefaultPassword is configured in registry."
+        Write-Verbose "Making sure DefaultPassword key is configured in registry" -Verbose
+
+    }
+    else {
+
+        #Create local user for KIOSK
+
+        New-LocalUser -Name $DefaultUsername -AccountNeverExpires:$true -UserMayNotChangePassword:$true -PasswordNeverExpires:$true -Password ( ConvertTo-SecureString -AsPlainText -Force $scrambledpassword ) -FullName $DefaultUsername -Description "Local KIOSK User" -ErrorAction Stop -Verbose
+
+        Add-Content $ConfigLog "$(Get-TimeStamp) - Local 'KIOSK' user have been created." -Verbose
+        Write-Verbose "User: $DefaultUsername was created" -Verbose
      
-    Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String -ErrorAction Stop -Verbose
-    Add-Content $ConfigLog "$(Get-TimeStamp) - AutoAdminLogin is configured." -Verbose
-    Write-Verbose "AutoAdminLogon enabled" -Verbose
+        Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String -ErrorAction Stop -Verbose
+        Add-Content $ConfigLog "$(Get-TimeStamp) - AutoAdminLogin is configured." -Verbose
+        Write-Verbose "AutoAdminLogon enabled" -Verbose
 
-    Set-ItemProperty $RegPath "DefaultUsername" -Value ".\$DefaultUsername" -type String  -ErrorAction Stop -Verbose
-    Add-Content $ConfigLog "$(Get-TimeStamp) - DefaultUsername is configured." -Verbose
-    Write-Verbose "$DefaultUsername is set as Defaultusername" -Verbose
+        Set-ItemProperty $RegPath "DefaultUsername" -Value ".\$DefaultUsername" -type String  -ErrorAction Stop -Verbose
+        Add-Content $ConfigLog "$(Get-TimeStamp) - DefaultUsername is configured." -Verbose
+        Write-Verbose "$DefaultUsername is set as Defaultusername" -Verbose
 
-    Set-ItemProperty $RegPath "DefaultPassword" -Value "$scrambledpassword" -type String -ErrorAction Stop -Verbose
-    Add-Content $ConfigLog "$(Get-TimeStamp) - DefaultPassword is configured." -Verbose
-    Write-Verbose "DefaultPassword is set" -Verbose
+        Set-ItemProperty $RegPath "DefaultPassword" -Value "$scrambledpassword" -type String -ErrorAction Stop -Verbose
+        Add-Content $ConfigLog "$(Get-TimeStamp) - DefaultPassword is configured." -Verbose
+        Write-Verbose "DefaultPassword is set" -Verbose
 
-    Add-Content $ConfigLog "$(Get-TimeStamp) - Local 'KIOSK' user is configured for AutoLogon."
-    Write-host "KIOSK configuration finish" -Verbose -BackgroundColor Cyan -ForegroundColor Black
+        Add-Content $ConfigLog "$(Get-TimeStamp) - Local 'KIOSK' user is configured for AutoLogon."
+        Write-host "KIOSK configuration finish" -Verbose -BackgroundColor Cyan -ForegroundColor Black
        
-    New-Item -Path $DetectionMethod -Force -Verbose -ErrorAction Stop #Versiosnumber needs to be updated each change
-    
-    Exit 0
+        New-Item -Path $DetectionMethod -Force -Verbose -ErrorAction Stop #Versiosnumber needs to be updated each change
+    } 
+    Exit 0  
 }
     
 Catch {   
