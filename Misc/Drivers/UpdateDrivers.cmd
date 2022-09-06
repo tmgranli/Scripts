ad51b028e3f74e6870a5e0f2d3ba53633fbcd68e
Truls Morten Granli <truls.granli@sarpsborg.com>
2022-09-06 13:11:55 +0200

    Changes dir

--- Misc/Drivers/UpdateDrivers.cmd
+++ Misc/Drivers/UpdateDrivers.cmd
@@ -1,2 +1,2 @@
-xcopy C:\Scripts\Misc\Drivers\WiFi-22.160.0\* C:\_SK\Lenovo500W\WiFi-22.160.0 /i
-pnputil /add-driver C:\_SK\Lenovo500W\WiFi-22.160.0\*.inf
+xcopy WiFi-22.160.0\* C:\_SK\Lenovo500W\WiFi-22.160.0 /i /y
+#pnputil /add-driver C:\_SK\Lenovo500W\WiFi-22.160.0\*.inf
