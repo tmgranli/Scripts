bf00f308f4c4ac0d3fdf971db387f82a1f3deab0
Truls Granli <tm.granli@gmail>
2022-10-01 13:26:24 +0100

    changes

--- VMware/Invoke-AddVlanToHost.ps1
+++ VMware/Invoke-AddVlanToHost.ps1
@@ -1,3 +1,3 @@
-Connect-VIServer labvm01.sarpsborg.com
+Connect-VIServer labvm01.local.host
 
 get-cluster -name datacenter01 | Get-VMHost | Get-VirtualSwitch -name "vSwitch1" | New-VirtualPortGroup -Name "Srv.SecureServers" -VLanId "1337"
