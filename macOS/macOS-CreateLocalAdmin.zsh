5a37e31eb1db56ccea1bdf6ee3e6de27d54664bf
Truls Granli <tm.granli@gmail.com>
2023-01-12 16:10:51 +0100

    macos scripts

---
+++ macOS/macOS-CreateLocalAdmin.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+# Username and Password to create
+username=dilldalladmin
+password="klient15klient15"
+# Create User and add to admins
+dscl . -create /Users/$username
+dscl . -create /Users/$username UserShell /bin/bash
+dscl . -create /Users/$username RealName $username
+dscl . -create /Users/$username UniqueID "510"
+dscl . -create /Users/$username PrimaryGroupID 20
+dscl . -create /Users/$username NFSHomeDirectory /Users/$username
+dscl . -passwd /Users/$username $password
+dscl . -append /Groups/admin GroupMembership $username
---
+++ macOS/macOS-Install-GlobalProtect-SAML.zsh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+plistBuddy='/usr/libexec/PlistBuddy'
+GPplistFile='/Library/Preferences/com.paloaltonetworks.GlobalProtect.settings.plist'
+
+if [ -f ${GPplistFile} ]
+then
+	echo "Plist already exists..."
+else
+	echo "Creating Plist"
+	${plistBuddy} -c "print : 'Palo Alto Networks':'GlobalProtect':'PanSetup':'Portal'" ${GPplistFile}
+	${plistBuddy} -c "add :'Palo Alto Networks' dict" ${GPplistFile}
+	${plistBuddy} -c "add :'Palo Alto Networks':'GlobalProtect' dict" ${GPplistFile}
+	${plistBuddy} -c "add :'Palo Alto Networks':'GlobalProtect':'PanSetup' dict" ${GPplistFile}
+	${plistBuddy} -c "add :'Palo Alto Networks':'GlobalProtect':'PanSetup':'Portal' string 'cvpn.sarpdev.com'" ${GPplistFile}
+	${plistBuddy} -c "add :'Palo Alto Networks':'GlobalProtect':'PanSetup':'Prelogon' integer 0" ${GPplistFile}
+fi
+	
+if [ -d "/Applications/GlobalProtect.app" ]
+then
+	echo "Already installed..."
+	exit 0
+else
+	echo "Preparing..."
+	curl "https://memtest.sarpdev.com/Test/GlobalProtect.pkg" > "/tmp/GlobalProtect.pkg"
+	echo "Installing..."	
+	sudo installer -pkg "/tmp/GlobalProtect.pkg" -target /
+	echo "Cleaning..."
+	rm "/tmp/GlobalProtect.pkg"
+	exit 0
+fi
